cc6119c3450cf9397df6e189c455b53bdff7504f
Yusuke_Saito <ysaito8015@gmail.com>
2020-09-08 23:24:53 +0200

    done ch02

---
+++ config/config.exs
@@ -0,0 +1,31 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :nano_planner,
+  ecto_repos: [NanoPlanner.Repo]
+
+# Configures the endpoint
+config :nano_planner, NanoPlannerWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "oUFN2cV2kfVDgWCVpth6ZqVIN2CkHAxXhlFXBWl88KjzHkFFA910UF8jwjDWB9ZO",
+  render_errors: [view: NanoPlannerWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: NanoPlanner.PubSub,
+  live_view: [signing_salt: "y3Ipgh+6"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ lib/nano_planner_web/router.ex
@@ -0,0 +1,15 @@
+defmodule NanoPlannerWeb.Router do
+  use NanoPlannerWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/", NanoPlannerWeb do
+    pipe_through :browser
+  end
+end
