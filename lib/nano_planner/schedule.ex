db2eada743eaf9793a02bb63a76faca550989c11
Yusuke_Saito <ysaito8015@gmail.com>
2020-09-26 01:16:12 +0200

    [WIP] create action

--- lib/nano_planner/schedule.ex
+++ lib/nano_planner/schedule.ex
@@ -37,6 +37,15 @@ defmodule NanoPlanner.Schedule do
     Application.get_env(:nano_planner, :default_time_zone)
   end
 
+  def create_plan_item(attrs) do
+    #attrs = %{attrs | "starts_at" => Timex.Timezone.convert(Timex.parse!(attrs["starts_at"], "{RFC3339}"), "Etc/UTC")}
+    #attrs = %{attrs | "ends_at" => Timex.Timezone.convert(Timex.parse!(attrs["ends_at"], "{RFC3339}"), "Etc/UTC")}
+    item = %PlanItem{}
+    fields = [:name, :description, :starts_at, :ends_at]
+    cs = Ecto.Changeset.cast(item, attrs, fields)
+    Repo.insert!(cs)
+  end
+
   def change_plan_item(%PlanItem{} = item) do
     PlanItem.changeset(item, %{})
   end
@@ -51,6 +60,17 @@ defmodule NanoPlanner.Schedule do
     Map.merge(item, %{
       starts_at: Timezone.convert(item.starts_at, time_zone()),
       ends_at: Timezone.convert(item.ends_at, time_zone())
+    })
+  end
+
+  defp convert_to_utc(attrs) do
+    alias Timex.Timezone
+
+    Map.merge(attrs, %{
+      "starts_at" => attrs["starts_at"]
+      |> Timezone.convert("Etc/UTC"),
+      "ends_at" => attrs["ends_at"]
+      |> Timezone.convert("Etc/UTC")
     })
   end
 end
